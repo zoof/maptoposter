5681c7af9a31fbdf5ec63d211ba82feb0721acd4
Ted To <ted@theo.to>
2026-03-08 18:00:21 -0400

    do everything in memory now so no need to save individual swatches

--- theme-swatches.py
+++ theme-swatches.py
@@ -3,13 +3,10 @@
 
 import json
 import seaborn as sns
-from os import listdir, path, mkdir
+from os import listdir
+from io import BytesIO
 import matplotlib.pyplot as plt
 from PIL import Image
-
-# check if directory "swatches" exists and if not, create it
-if not path.exists('swatches'):
-    os.mkdir('swatches')
 
 def invert_hex(h):
     """
@@ -38,48 +35,47 @@ def draw_swatch(colors,ax):
 # Initialize seaborn
 sns.set()
 
+sw_width = 1084
+sw_height = 100
+
+theme_list = listdir('themes')
+
+width = sw_width
+height = sw_height*len(theme_list)
+swatches = Image.new('RGB', (width, height))
+y_offset = 0
+
 # Loop through all themes, drawing a swatch for each theme
-for theme_file in listdir('themes'):
+for theme_file in theme_list:
     with open('themes/' + theme_file) as th:
         # load theme
         theme = json.load(th)
-        # extract theme name and delete
-        theme_name = theme['name']
-        del(theme['name'])
-        # extract theme description and delete
-        theme_description = theme['description']
-        del(theme['description'])
+    # extract theme name and delete
+    theme_name = theme.pop('name')
+    theme_description = theme.pop('description')
 
-		# Draw swatch
-        sns.palplot(theme.values())
+	# Draw swatch
+    sns.palplot(theme.values())
 
-		# Add title to swatch
-        plt.title(theme_name)
-        # Add key and hexadecimal color
-        for i, key in enumerate(theme):
-            plt.text(
-                i, 0.2,
-                key+'\n'+theme[key],
-                ha='center',
-                va='bottom',
-                color=invert_hex(theme[key]),
-                fontsize=7
-            )
-        plt.tight_layout()
-        plt.savefig('swatches/'+theme_name+'.png')
+	# Add title to swatch
+    plt.title(theme_name)
+    # Add key and hexadecimal color
+    for i, key in enumerate(theme):
+        plt.text(
+            i, 0.2,
+            key+'\n'+theme[key],
+            ha='center',
+            va='bottom',
+            color=invert_hex(theme[key]),
+            fontsize=7
+        )
+    plt.tight_layout()
+    buf = BytesIO()
+    plt.savefig(buf)
+    buf.seek(0)
 
-# read in swatches
-swatches = [Image.open('swatches/'+swf) for swf in listdir('swatches')]
-# extract widths and heights
-widths, heights = zip(*(sw.size for sw in swatches))
-# compute dimensions of new image
-max_width = max(widths)
-total_height = sum(heights)
-
-# create composite image
-swatches_im = Image.new('RGB', (max_width, total_height))
-y_offset = 0
-for sw in swatches:
-    swatches_im.paste(sw, (0,y_offset))
-    y_offset += sw.size[1]
-swatches_im.save('theme_swatches.png')
+    sw = Image.open(buf)
+    swatches.paste(sw, (0,y_offset))
+    y_offset += sw_height
+    plt.close()
+swatches.save('theme_swatches.png')
